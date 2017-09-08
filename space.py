abb12a666692b42a67b64453d3fee51d23ff80d5
nuttapon <nuttapon.wac@ku.th>
2017-09-08 11:18:15 +0700

    update class World in models

--- space.py
+++ space.py
@@ -1,6 +1,6 @@
 import arcade
 
-from models import Ship
+from models import Ship,World
  
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
@@ -13,12 +13,11 @@ class SpaceGameWindow(arcade.Window):
 
         
         self.ship_sprite = arcade.Sprite('images/ship.png')
-        self.ship = Ship(100,100)
+        self.world = World(width, height)
 
     def update(self, delta):
-    	ship = self.ship
-    	ship.update(delta)
-    	self.ship_sprite.set_position(ship.x, ship.y)
+        self.world.update(delta)
+        self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
  
  
     def on_draw(self):
